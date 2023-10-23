44553924bd7c5aa363906c59b8b25cf7a2420714
LucaGiuseppeLazzeri <lgl105@student.aru.ac.uk>
2023-10-23 18:40:21 +0100

    A C# login page using sql

---
+++ Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace LoginForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button_login = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            txt_username = new TextBox();
+            txt_password = new RichTextBox();
+            SuspendLayout();
+            // 
+            // button_login
+            // 
+            button_login.Location = new Point(226, 223);
+            button_login.Name = "button_login";
+            button_login.Size = new Size(75, 23);
+            button_login.TabIndex = 0;
+            button_login.Text = "Login";
+            button_login.UseVisualStyleBackColor = true;
+            button_login.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(431, 226);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 1;
+            button2.Text = "Clear";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(55, 330);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 23);
+            button3.TabIndex = 2;
+            button3.Text = "exit";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(55, 81);
+            label1.Name = "label1";
+            label1.Size = new Size(60, 15);
+            label1.TabIndex = 3;
+            label1.Text = "Username";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(55, 124);
+            label2.Name = "label2";
+            label2.Size = new Size(57, 15);
+            label2.TabIndex = 4;
+            label2.Text = "Password";
+            // 
+            // txt_username
+            // 
+            txt_username.Location = new Point(135, 81);
+            txt_username.Name = "txt_username";
+            txt_username.Size = new Size(342, 23);
+            txt_username.TabIndex = 5;
+            txt_username.TextChanged += textBox1_TextChanged;
+            // 
+            // txt_password
+            // 
+            txt_password.Location = new Point(135, 124);
+            txt_password.Name = "txt_password";
+            txt_password.Size = new Size(342, 23);
+            txt_password.TabIndex = 6;
+            txt_password.Text = "";
+            txt_password.TextChanged += richTextBox1_TextChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(txt_password);
+            Controls.Add(txt_username);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button_login);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button_login;
+        private Button button2;
+        private Button button3;
+        private Label label1;
+        private Label label2;
+        private TextBox txt_username;
+        private RichTextBox txt_password;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoginForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection conn = new SqlConnection(@"Data Source=DESKTOP-37PB8E2\SQLEXPRESS;Initial Catalog=Login;Integrated Security=True");
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_login_Click(Object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String username, user_password;
+
+            username = txt_username.Text;
+            user_password = txt_password.Text;
+
+            try
+            {
+                String querry = "SELECT * FROM Lognin WHERE username = '" + txt_username.Text + "' AND password = '" + txt_password.Text + "'";
+                SqlDataAdapter sda = new SqlDataAdapter(querry, conn);
+
+                DataTable dtable = new DataTable();
+                sda.Fill(dtable);
+
+                if (dtable.Rows.Count > 0)
+                {
+                    username = txt_username.Text;
+                    user_password = txt_password.Text;
+
+                    //page that loads next
+                    Menuform form2 = new Menuform();
+                    form2.Show();
+                    // this..dosent work this.Hide;
+                }
+                else
+                {
+                    MessageBox.Show("Invaild login", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    txt_username.Clear();
+                    txt_password.Clear();
+
+                    txt_username.Focus();
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Error");
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            txt_username.Clear();
+            txt_password.Clear();
+
+            txt_username.Focus();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DialogResult res;
+            res = MessageBox.Show("Do you want to exit?","Exit", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (res == DialogResult.Yes)
+            {
+                Application.Exit;
+            }
+            else
+            {
+                this.Show();
+            }
+
+        }
+    };
+}
